864077983b37682625a252b46ac1475badace906
Rupinder Monga <rupindermonga@gmail.com>
2019-09-06 15:41:35 +0530

    Configuring Rails API

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_auth_app", domain: "https:rupi-auth-app-api.herokuapp.com"
+if Rails.env == "production"
+    Rails.application.config.session_store :cookie_store, key: "_auth_app", domain: "https:rupi-auth-app-api.herokuapp.com"
+else
+    Rails.application.config.session_store :cookie_store, key: "_auth_app"
+end
