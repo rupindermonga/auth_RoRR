c67a29f02b09ee405fd19e070173bc7dcccd3abc
Rupinder Monga <rupindermonga@gmail.com>
2019-10-05 20:39:32 +0530

    uploaded sample dataset

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_06_070638) do
+ActiveRecord::Schema.define(version: 2019_10_05_150637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "houses", force: :cascade do |t|
+    t.float "latitude"
+    t.float "longitude"
+    t.text "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
